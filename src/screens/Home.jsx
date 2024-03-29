99f502c36370c50705fc1a8cf3a5d46448c2a532
om <omchevli@gmail.com>
2024-03-28 21:32:31 -0400

    added home page header

--- src/screens/Home.jsx
+++ src/screens/Home.jsx
@@ -1,5 +1,11 @@
-const Home = () =>{
+import Header from "../components/header/Header";
 
-}
+const Home = () => {
+  return (
+    <div>
+        <Header />
+    </div>
+  );
+};
 
 export default Home;
